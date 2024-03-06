8ec4220e9f1afa88319ec0d856ac56de2a9db068
minimini728 <113008977+minimini728@users.noreply.github.com>
2024-03-06 23:35:43 +0900

    Add files via upload

---
+++ RabbitPlanet/Admob/RestartButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartButton : MonoBehaviour
+{
+    //public FullAdmob admobManager; // FullAdmob 스크립트에 대한 참조를 설정하기 위한 변수
+
+    private void Start()
+    {
+        //admobManager = FindObjectOfType<FullAdmob>(); // FullAdmob 스크립트를 찾아서 참조를 설정합니다.
+    }
+
+    public void LoadAndShowInterstitialAd()
+    {
+        // Load and show the interstitial ad using the FullAdmob script
+        //admobManager.LoadLoadInterstitialAd();
+        //admobManager.ShowInterstitialAd();
+    }
+}
